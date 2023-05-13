35fdfd045a7e33685c0b1e2034ed101fc0390c2d
Claudia Gillota <cgillota@gmail.com>
2023-05-12 21:49:34 -0400

    started server.js & inquirer prompts

---
+++ server.js
@@ -0,0 +1,91 @@
+//Import and require
+const inquirer = require('inquirer');
+const mysql = require('mysql2');
+
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      user: 'root',
+      password: 'Carmella07*',
+      database: 'employees_db'
+    },
+    console.log(`Connected to the employees_db database.`)
+  ); 
+
+  db.connect((err) => {
+    if (err) {
+        throw err;
+    }
+    console.log("Mysql Connected"); 
+    start();
+  });
+
+
+  function start() {
+    inquirer
+        .prompt({
+            type: "list",
+            name: "action",
+            message: "What would you like to do?",
+            choices: [
+                "View all departments",
+                "View all roles",
+                "View all employees",
+                "Add a department",
+                "Add a role",
+                "Add an employee",
+                "Update an employee role",
+                //for later if you get it and have time to figure it out
+                // "Add a Manager",
+                //"View Employees by Manager",
+                //"View Employees by Department",
+                //"Delete Departments | Roles | Employees",
+                //"View the total utilized budget of a department",
+                "Exit",
+            ],
+        })
+        .then((answer) => {
+            switch (answer.action) {
+                case "View all departments":
+                    viewAllDepartments();
+                    break;
+                case "View all roles":
+                    viewAllRoles();
+                    break;
+                case "View all employees":
+                    viewAllEmployees();
+                    break;
+                case "Add a department":
+                    addDepartment();
+                    break;
+                case "Add a role":
+                    addRole();
+                    break;
+                case "Add an employee":
+                    addEmployee();
+                    break;
+                case "Add a Manager":
+                    addManager();
+                    break;
+                case "Update an employee role":
+                    updateEmployeeRole();
+                    break;
+                case "View Employees by Manager":
+                    viewEmployeesByManager();
+                    break;
+                case "View Employees by Department":
+                    viewEmployeesByDepartment();
+                    break;
+                case "Delete Departments | Roles | Employees":
+                    deleteDepartmentsRolesEmployees();
+                    break;
+                case "View the total utilized budget of a department":
+                    viewTotalUtilizedBudgetOfDepartment();
+                    break;
+                case "Exit":
+                    connection.end();
+                    console.log("Goodbye!");
+                    break;
+            }
+        });
+}
